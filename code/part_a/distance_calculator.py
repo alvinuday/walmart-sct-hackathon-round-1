4fb9ea71b231c09479a36f9e4a74ea0b93b2ca06
xFBIx <rudradattdave@gmail.com>
2024-03-20 20:23:38 +0530

    fixes

--- code/part_a/distance_calculator.py
+++ code/part_a/distance_calculator.py
@@ -13,3 +13,11 @@ def calculate_distance(lat1, lon1, lat2, lon2):
     distance = round(distance, 2)
 
     return distance
+
+
+lat1, lon1 = 43.8121, 126.5669
+lat2, lon2 = 43.81811, 126.55716
+
+# Calculate distance
+distance = calculate_distance(lat1, lon1, lat2, lon2)
+print("Traveling distance:", distance, "kms")
